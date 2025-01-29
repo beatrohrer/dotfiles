269d22656b616ab07639648074eb91f5b98a7965
Beat Rohrer <beat.rohrer@zhdk.ch>
2025-01-29 15:03:19 +0100

    Fix script

--- scripts/update_lamp_dbs.sh
+++ scripts/update_lamp_dbs.sh
@@ -5,7 +5,8 @@ if [ -z "${1-}" ]; then
    exit 1
 fi
 
-ssh largo '/var/lib/zhdk/scripts/export_db.sh';
+ssh largo '/var/lib/zhdk/scripts/export_db.sh' > local_output;
+echo "done exporting DBs"
 scp -3q largo:/var/lib/zhdk/db-dumps/hmtfwdb_latest.sql lamp:~/tmp/hmtfwdb_latest.sql &
 scp -3q largo:/var/lib/zhdk/db-dumps/typo3db_latest.sql lamp:~/tmp/typo3db_latest.sql &
 wait
@@ -16,6 +17,8 @@ ssh lamp "mysql -uroot -p$1 zhdk_hmtfw < ~/tmp/hmtfwdb_latest.sql" &
 ssh lamp "mysql -uroot -p$1 zhdk_typo3 < ~/tmp/typo3db_latest.sql" &
 wait
 echo "done importing…"
+echo "cleaning up imported DB"
+ssh lamp "mysql -uroot -p$1 -e 'USE zhdk_hmtfw; DELETE FROM raum_reservation WHERE created_date < DATE_ADD(NOW(), INTERVAL -3 MONTH);'"
 echo "starting cleanup…"
 ssh lamp 'rm ~/tmp/hmtfwdb_latest.sql ~/tmp/typo3db_latest.sql';
 echo "done"
